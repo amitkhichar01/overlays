47f447c8e314f8540dd8b375a273c0a194166570
amitkhichar01git config --global  user.email amitkhichar100@gmail.com <amitkhichar100@gmail.com>
2024-01-03 14:57:43 +0530

    fix app.js code

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    store: new MongoStore({ url: dbUrl }),
+    store: MongoStore.create({ mongoUrl: dbUrl }),
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
